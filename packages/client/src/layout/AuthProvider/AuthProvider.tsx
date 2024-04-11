96fc1227cf13d4b91432e4081332f82e5a43ad81
Wonder Worker <saintnikfreak@gmail.com>
2024-04-11 15:27:45 +0300

    fix loading

--- packages/client/src/layout/AuthProvider/AuthProvider.tsx
+++ packages/client/src/layout/AuthProvider/AuthProvider.tsx
@@ -1,5 +1,35 @@
+// import { useState, useEffect } from 'react';
+// import { useSession } from 'next-auth/react';
+// import baseApi from '@/lib/baseApi';
+
+// import { IParent } from '@/interfaces/components.interfaces';
+// import { Spinner } from '@/components/ui/Spinner';
+
+// export const AuthProvider = ({ children }: IParent) => {
+//   const { data: session, status } = useSession();
+//   const [isSessionLoading, setIsSessionLoading] = useState(true);
+
+//   useEffect(() => {
+//     if (status === 'loading') return; // Session is still loading
+//     if (session) {
+//       baseApi.defaults.headers.common['Authorization'] =
+//         `Bearer ${session?.user?.token}`;
+//     }
+//     setIsSessionLoading(false);
+//   }, [session, status]);
+
+//   if (isSessionLoading) {
+//     return (
+//       <div className="w-screen h-screen flex items-center justify-center">
+//         <Spinner className="w-12 h-12" />
+//       </div>
+//     );
+//   }
+
+//   return <>{children}</>;
+// };
 import { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
+import { useSession, getSession } from 'next-auth/react';
 import baseApi from '@/lib/baseApi';
 
 import { IParent } from '@/interfaces/components.interfaces';
@@ -10,12 +40,27 @@ export const AuthProvider = ({ children }: IParent) => {
   const [isSessionLoading, setIsSessionLoading] = useState(true);
 
   useEffect(() => {
-    if (status === 'loading') return; // Session is still loading
-    if (session) {
-      baseApi.defaults.headers.common['Authorization'] =
-        `Bearer ${session?.user?.token}`;
-    }
-    setIsSessionLoading(false);
+    const interval = setInterval(() => {
+      if (status !== 'loading') {
+        if (session) {
+          baseApi.defaults.headers.common['Authorization'] =
+            `Bearer ${session?.user?.token}`;
+        }
+        setIsSessionLoading(false);
+        clearInterval(interval);
+      } else {
+        getSession().then(session => {
+          if (session) {
+            baseApi.defaults.headers.common['Authorization'] =
+              `Bearer ${session?.user?.token}`;
+            setIsSessionLoading(false);
+            clearInterval(interval);
+          }
+        });
+      }
+    }, 1000); // Check every second
+
+    return () => clearInterval(interval); // Clean up on unmount
   }, [session, status]);
 
   if (isSessionLoading) {
